19eb42774ba084eeb609f93b3db7b3f8b78eab93
AlissonRP <alirpereira887@gmail.com>
2022-07-11 03:06:40 -0300

    bisection method

---
+++ zero_functions/bisection/bisection.jl
@@ -0,0 +1,30 @@
+using Plots
+f(x) = x * log(x) - 1
+
+"""
+    bisection(g, a, b, error)
+
+    g function to find the zero
+    a begin of interval
+    b end of interval
+    error is the ERROR!
+"""
+bisection = function (g::Function, a::Real, b::Real; error::Real = 0.001)
+    θ = (a + b) / 2
+    n = 0
+    while abs(g(θ)) > error && n < 500
+        θ = (a + b) / 2
+        if g(a) * g(θ) < 0
+            b = θ
+        else
+            a = θ
+        end
+        n += 1
+    end
+    return a, b, θ, n, g(θ) 
+end
+        
+
+
+bisection(f, 1, 2)
+
